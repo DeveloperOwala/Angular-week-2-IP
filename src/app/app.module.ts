24989bbbb54597a9bd692085c56a8cd8a8a1576c
DeveloperOwala <otienoerick46@gmail.com>
2021-08-28 19:01:28 +0300

    adding user class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
+import { UsersComponent } from './users/profile.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/users/profile.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  profile:any;
+  repos:any;
+  username:string;
+
+constructor(private profileService:ProfileService){
+  this.profileService.updateProfile(this.username);
+  this.profileService.getProfileInfo().subscribe(profile => {
+    console.log(profile);
+    this.profile = profile;
+  });
+
+  this.profileService.getProfileRepos().subscribe(repos => {
+    console.log(repos);
+    this.repos = repos;
+  });
+}
+ 
+findProfile(){
+  this.profileService.updateProfile(this.username);
+  this.profileService.getProfileInfo().subscribe(profile => {
+    console.log(profile);
+    this.profile = profile;
+  });
+
+  this.profileService.getProfileRepos().subscribe(repos => {
+    console.log(repos);
+    this.repos = repos;
+  })
+}
+
+  ngOnInit() {
+    this.profileService.updateProfile('MUTHAKABRIAN');
+    
+    this.profileService.getProfileInfo().subscribe(profile => this.profile = profile);
+
+    this.profileService.getProfileRepos().subscribe(repos =>  this.repos = repos);
+  }
+
+
+}

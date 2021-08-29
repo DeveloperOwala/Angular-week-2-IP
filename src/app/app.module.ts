d0b4eda8c88b8454c786ae2f12087423a5458b51
DeveloperOwala <otienoerick46@gmail.com>
2021-08-29 16:29:58 +0300

    profile component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import{HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/profile.component';
+import { ProfileComponent } from './profile/profile.component';
+import { DateCountPipe } from './date-count.pipe';
+import { BorderDirective } from './border.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    ProfileComponent,
+    DateCountPipe,
+    BorderDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
   private username:any;
   private clientid!: "8035273870d0778107b0";
-  private clientsecret:"b4aee1eba8670d25a47c4480e9484711764a3548"
+  private clientsecret!:"b4aee1eba8670d25a47c4480e9484711764a3548"
   constructor(private http:HttpClient) { 
     this.username ="MUTHAKABRIAN"
   }
